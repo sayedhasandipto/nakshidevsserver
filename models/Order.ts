39c1d71d9be260c597144d1795b4244950bd151a
Sayed Hasan Dipto <sayedhasandipto@gmail.com>
2026-07-25 21:52:42 +0600

    fixning order url

--- models/Order.ts
+++ models/Order.ts
@@ -1,7 +1,6 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IOrder extends Document {
-  orderId: string;
   customerName: string;
   customerEmail: string;
   serviceName: string;
@@ -11,13 +10,17 @@ export interface IOrder extends Document {
 }
 
 const OrderSchema: Schema = new Schema({
-  orderId: { type: String, required: true, unique: true },
   customerName: { type: String, required: true },
   customerEmail: { type: String, required: true },
   serviceName: { type: String, required: true },
   amount: { type: String, required: true },
-  status: { type: String, default: 'Pending', enum: ['Pending', 'In Progress', 'Completed', 'Cancelled'] },
+  status: {
+    type: String,
+    default: "Pending",
+    enum: ["Pending", "In Progress", "Completed", "Cancelled"],
+  },
   orderDate: { type: Date, default: Date.now },
 });
 
-export default mongoose.models.Order || mongoose.model<IOrder>('Order', OrderSchema);
+export default mongoose.models.Order ||
+  mongoose.model<IOrder>("Order", OrderSchema);
